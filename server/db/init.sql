e48820401fafaf1859bf6750ba53878863091569
Joe <32417992+j-snowden98@users.noreply.github.com>
2019-10-29 22:32:36 +0000

    Draft of database set up

---
+++ server/db/init.sql
@@ -0,0 +1,58 @@
+create database if not exists st_ronans_care;
+
+CREATE TABLE IF NOT EXISTS st_ronans_care.User (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(100),
+  password VARCHAR(100),
+  role VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS st_ronans_care.Permissions (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS st_ronans_care.UserPermissions (
+  userID INT NOT NULL,
+  pmsnID INT NOT NULL,
+  FOREIGN KEY (userID) REFERENCES User(id),
+  FOREIGN KEY (pmsnID) REFERENCES Permissions(id)
+);
+
+CREATE TABLE IF NOT EXISTS st_ronans_care.Resident (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  forename VARCHAR(100) NOT NULL,
+  surname VARCHAR(100) NOT NULL,
+  dietReq VARCHAR(100) NOT NULL,
+  allergies VARCHAR(100) NOT NULL,
+  thickener BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS st_ronans_care.Room (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  roomName VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS st_ronans_care.ResidentRoom (
+  resID INT NOT NULL,
+  roomID INT NOT NULL,
+  FOREIGN KEY (resID) REFERENCES Resident(id),
+  FOREIGN KEY (roomID) REFERENCES Room(id)
+);
+
+CREATE TABLE IF NOT EXISTS st_ronans_care.Contact (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  resID INT NOT NULL,
+  userID INT NOT NULL,
+  contactDate DATE NOT NULL,
+  contactTime TIME NOT NULL,
+  callBell BOOLEAN NOT NULL,
+  drinkGiven BOOLEAN NOT NULL,
+  description TEXT,
+  FOREIGN KEY (resID) REFERENCES Resident(id),
+  FOREIGN KEY (userID) REFERENCES User(id)
+);
+
+INSERT INTO st_ronans_care.User (username, password, role)
+VALUES
+    ('j-snowden', '$2a$10$cz4RgHPRI2xfd4jqn2vXFeJfThfcHqtITtLWKkgeSLPn4WjGUU5eG', 'Administrator');
