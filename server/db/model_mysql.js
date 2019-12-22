fa4f1c410595bfe5895e2c8f09f2a6f41cbb5c4a
Joe <32417992+j-snowden98@users.noreply.github.com>
2019-12-22 20:12:48 +0000

    Merged date and time into 1 datetime field to aid searching and sorting. Updated init script with DB changes.

--- server/db/model_mysql.js
+++ server/db/model_mysql.js
@@ -58,7 +58,7 @@ async function getResidents(search) {
 
 async function searchContact(resID) {
   const sql = await init();
-  const query = sql.format('SELECT U.username, C.contactDate, C.contactTime, C.callBell, C.drinkGiven, C.description FROM Contact C LEFT JOIN User U ON U.id = C.userID WHERE C.resID = ? AND C.contactDate >= CURDATE() - INTERVAL 1 DAY ORDER BY C.contactDate, C.contactTime DESC', [resID]);
+  const query = sql.format('SELECT U.username, DATE_FORMAT(DATE(C.contactDate), "%d/%m/%Y") as contactDate, DATE_FORMAT(TIME(C.contactDate), "%H:%i") as contactTime, C.callBell, C.drinkGiven, C.description FROM Contact C LEFT JOIN User U ON U.id = C.userID WHERE C.resID = ? AND C.contactDate >= NOW() - INTERVAL 1 DAY ORDER BY C.contactDate DESC', [resID]);
   const [rows] = await sql.query(query);
   return (rows);
 }
