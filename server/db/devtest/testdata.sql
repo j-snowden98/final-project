f925840967706f7b1cdc6cf16fe275b41c3f784d
j-snowden98 <32417992+j-snowden98@users.noreply.github.com>
2020-01-12 18:16:23 +0000

    Test data for residents and rooms. Still needs some users and permissions.

--- server/db/devtest/testdata.sql
+++ server/db/devtest/testdata.sql
@@ -1,33 +1,33 @@
-INSERT INTO st_ronans_care.Resident (forename, surname, dietReq, allergies, thickener)
+INSERT INTO st_ronans_care.Resident (forename, surname, dietReq, allergies, thickener, diabetes, dnr)
 VALUES
-  ('Steve', 'Test1', '', '', true),
-  ('Larry', 'Test2', '', '', true),
-  ('Bob', 'Test3', '', '', true),
-  ('Barry', 'Test4', '', '', true),
-  ('Sally', 'Test5', '', '', true),
-  ('Ethel', 'Test6', '', '', true);
+  ('TestF1', 'TestS1', '', '', true, 2, true),
+  ('TestF2', 'TestS2', '', '', false, 0, true),
+  ('TestF3', 'TestS3', '', '', false, 1, false),
+  ('TestF4', 'TestS4', '', '', true, 2, false),
+  ('TestF5', 'TestS5', '', '', true, 0, false),
+  ('TestF6', 'TestS6', '', '', false, 2, true);
 
 INSERT INTO st_ronans_care.Room (roomName)
 VALUES
-  ('Room 1'),
-  ('Room 2'),
-  ('Room 3'),
-  ('Room 4'),
-  ('Room 5'),
-  ('Room 6'),
-  ('Room 7'),
-  ('Room 8'),
-  ('Room 9'),
-  ('Room 10'),
-  ('Room 11'),
-  ('Room 12'),
-  ('Room 13');
+  ('A1'),
+  ('A2'),
+  ('A3'),
+  ('A4'),
+  ('A201'),
+  ('B1'),
+  ('B2'),
+  ('B3'),
+  ('B21'),
+  ('B5'),
+  ('C1'),
+  ('C3'),
+  ('C200');
 
 INSERT INTO st_ronans_care.ResidentRoom (resID, roomID)
 VALUES
-  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'Test1'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'Room 4')),
-  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'Test2'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'Room 2')),
-  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'Test3'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'Room 1')),
-  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'Test4'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'Room 3')),
-  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'Test5'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'Room 10')),
-  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'Test6'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'Room 13'));
+  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'TestS1'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'A4')),
+  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'TestS2'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'B2')),
+  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'TestS3'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'A1')),
+  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'TestS4'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'B3')),
+  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'TestS5'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'C200')),
+  ((SELECT id FROM st_ronans_care.Resident WHERE surname = 'TestS6'), (SELECT id FROM st_ronans_care.Room WHERE roomName = 'A201'));
