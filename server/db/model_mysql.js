81c4a609a51af7dd0e1919398ac99a7eb1568931
j-snowden98 <32417992+j-snowden98@users.noreply.github.com>
2020-01-24 16:11:46 +0000

    Fix searchContact
    Found in test that the order of contact retrieved is incorrect if there are entries added within the same second. Also ordering by id afterwards, to ensure it is robust. Although it is unlikely that two contact entries would be added in one second.

--- server/db/model_mysql.js
+++ server/db/model_mysql.js
@@ -58,7 +58,7 @@ async function getResidents(search) {
 //Retrieves all contact sheet entries from the last 24hrs for a given resident.
 async function searchContact(resID) {
   const sql = await init();
-  const query = sql.format('SELECT U.username, DATE_FORMAT(DATE(C.contactDate), "%d/%m/%Y") as contactDate, DATE_FORMAT(TIME(C.contactDate), "%H:%i") as contactTime, C.callBell, C.drinkGiven, C.description, C.mood FROM Contact C LEFT JOIN User U ON U.id = C.userID WHERE C.resID = ? AND C.contactDate >= NOW() - INTERVAL 1 DAY ORDER BY C.contactDate DESC', [resID]);
+  const query = sql.format('SELECT U.username, DATE_FORMAT(DATE(C.contactDate), "%d/%m/%Y") as contactDate, DATE_FORMAT(TIME(C.contactDate), "%H:%i") as contactTime, C.callBell, C.drinkGiven, C.description, C.mood FROM Contact C LEFT JOIN User U ON U.id = C.userID WHERE C.resID = ? AND C.contactDate >= NOW() - INTERVAL 1 DAY ORDER BY C.contactDate DESC, C.id DESC', [resID]);
   const [rows] = await sql.query(query);
   return (rows);
 }
