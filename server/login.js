90612d5929183a333b4f4250bd174fdb43b2b91b
Joe <32417992+j-snowden98@users.noreply.github.com>
2020-01-31 17:53:11 +0000

    Change user.js to standalone login API

--- server/login.js
+++ server/login.js
@@ -9,32 +9,8 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.post('/register', async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  const role = req.body.role;
 
-  try {
-    //Creates hashed password to be stored in DB
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(password, salt, async function(err, hash) {
-        if(!err) {
-          console.log(hash);
-          //Uses DB model to save user with credentials and role to DB
-          await data.addUser(username, hash, role);
-          res.status(200).send({ 'user': username });
-        }
-      });
-    });
-  }
-  catch (e) {
-    console.log(e);
-    return res.status(500).send('Server error!');
-  }
-
-});
-
-router.post('/login', async (req, res) => {
+router.post('/', async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
