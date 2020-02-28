e5494539901ecb1034a08166e0415b4d8fe8e30b
Joe <32417992+j-snowden98@users.noreply.github.com>
2020-02-28 16:54:00 +0000

    Added route for room searching in admin page

--- server/manageRoom.js
+++ server/manageRoom.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const data = require('./db/model_mysql.js');
+const data = require('./db/admin_model_mysql.js');
 
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
+
+router.get('/search', async (req, res) => {
+  const filter = req.query.filter;
+
+  try {
+    const rooms = await data.searchRooms(filter);
+
+    res.status(200).json({ rooms: rooms });
+  }
+  catch (e) {
+    //Send response with error to client.
+    console.log(e);
+    return res.status(500).send('Server error!');
+  }
+});
 
 module.exports = router;
