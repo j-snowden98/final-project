221bd2ae5d5b88a2ca8f19020bd7b8412a8c4f07
Joe <32417992+j-snowden98@users.noreply.github.com>
2020-02-28 21:22:00 +0000

    For users returning to the site with a token, their admin and reports access will be sent back to them when residents are loaded.

--- server/searchResidents.js
+++ server/searchResidents.js
@@ -8,12 +8,16 @@ router.get('/', async (req, res) => {
   //Take username from decoded JWT access token
   const username = req.decoded.username;
   const filter = req.query.filter;
+  const userID = req.decoded.id;
 
   try {
     const residents = await data.getResidents(filter);
+    const adminAccess = await data.isAuthorised(userID, 5);
+    const reportAccess = await data.isAuthorised(userID, 6);
+    
     //Sends array of residents with info. Also sends username back to be added to navbar (when user is already logged in but refreshes the page).
     //Sends success which can be checked by client.
-    res.status(200).json({ residents: residents, username: username });
+    res.status(200).json({ residents: residents, username: username, admin: adminAccess, report: reportAccess });
   }
   catch (e) {
     //Send response with error to client.
