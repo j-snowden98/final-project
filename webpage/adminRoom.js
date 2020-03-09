41d6c9b053b42d0b6532b22975eb8286b72a274e
Joe <32417992+j-snowden98@users.noreply.github.com>
2020-03-09 19:00:55 +0000

    Added functionality for unassigning a resident from a room

--- webpage/adminRoom.js
+++ webpage/adminRoom.js
@@ -100,3 +100,64 @@ class ManageRoom {
     }
   }
 
+  clickUnassign() {
+    if (this.residentList.selectedIndex >= 0) {
+      const resName = this.residentList.value;
+
+      //Verfiy that the user wants to unassign the selected resident from the room
+      if(confirm('Are you sure you want to unassign ' + resName + ' from this room?')) {
+        this.unassignResident.bind(this)();
+      }
+    }
+    else {
+      window.alert('No resident has been selected');
+    }
+
+  }
+
+  async unassignResident() {
+    try {
+      const resID = this.residentList.options[this.residentList.selectedIndex].dataset.resid;
+      console.log(resID);
+      const response = await fetch(url + '/api/admin/room/unassign', {
+        method: 'POST',
+        body: JSON.stringify({ roomID: this.roomID, resID: resID }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const status = await response.status;
+
+      if(status === 200) {
+        //If resident has sucessfully been unassigned, refresh the list of assigned residents with the array given in the response
+        const json = await response.json();
+        this.updateResList(await json.residents);
+        this.resChanged = true;
+      }
+      else if(status === 401) {
+        //Hide form before asking user to log in again
+        this.hide();
+
+        //Forcelogin will call this function again, another attempt will be made to unassign the resident
+        this.retry = this.unassignResident.bind(this);
+        forceLogin.bind(this)();
+      }
+      else if(status === 403) {
+        //Notify the user that they are not authorised. Reloads the page as they should not be on the admin page
+        window.alert(await response.text());
+        location.reload();
+      }
+      else if (status === 500) {
+        clearError();
+        //The form may have been hidden due to the user being forced to log in on the last attempt
+        this.show();
+
+        //Notify user there has been an error. Leaves the form as it is in case they want to try again and keep the data.
+        this.container.insertAdjacentHTML('afterBegin', `<div id="errorAlert" class="alert alert-warning" role="alert">There was an error. Please try again later.
+        </div>`);
+      }
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
