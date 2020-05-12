a6d7287c12be9bffeee775df11d06e756c6a210e
Joe <32417992+j-snowden98@users.noreply.github.com>
2020-05-12 19:13:58 +0100

    Tests for stage 3 are complete

--- test/stage3.js
+++ test/stage3.js
@@ -120,10 +120,187 @@ describe('Stage 3 persistence tests', () => {
 
       let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
       report.should.have.lengthOf(12);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
       for (let s of report) {
         s.resName.should.deep.equal('Morris Moss');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
       }
+    });
 
+    it('Get all contact sheets for resident Edwin Eddison between the dates and times', async() => {
+      let userFilter = '';
+      let resFilter = 'Edwin Eddison';
+      let afterdate = '2000-01-01';
+      let afterTime = '00:00';
+      let beforeDate = '2020-05-01';
+      let beforeTime = '23:59';
+      let drinkGiven = 'false';
+      let moodFilter = '';
+      let orderBy = 0;
+
+      let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
+      report.should.have.lengthOf(9);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
+      for (let s of report) {
+        s.resName.should.deep.equal('Edwin Eddison');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
+      }
+    });
+
+    it('Get all contact sheets written by user test1 between the dates and times', async() => {
+      let userFilter = 'test1';
+      let resFilter = '';
+      let afterdate = '2000-01-01';
+      let afterTime = '00:00';
+      let beforeDate = '2020-05-01';
+      let beforeTime = '23:59';
+      let drinkGiven = 'false';
+      let moodFilter = '';
+      let orderBy = 0;
+
+      let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
+      report.should.have.lengthOf(5);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
+      for (let s of report) {
+        s.username.should.deep.equal('test1');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
+      }
+    });
+
+    it('Get all contact sheets written by user test3 between the dates and times', async() => {
+      let userFilter = 'test3';
+      let resFilter = '';
+      let afterdate = '2000-01-01';
+      let afterTime = '00:00';
+      let beforeDate = '2020-05-01';
+      let beforeTime = '23:59';
+      let drinkGiven = 'false';
+      let moodFilter = '';
+      let orderBy = 0;
+
+      let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
+      report.should.have.lengthOf(6);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
+      for (let s of report) {
+        s.username.should.deep.equal('test3');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
+      }
+    });
+
+    it('Get all contact sheets written by user test1 for resident Morris Moss between the dates and times', async() => {
+      let userFilter = 'test1';
+      let resFilter = 'Morris Moss';
+      let afterdate = '2000-01-01';
+      let afterTime = '00:00';
+      let beforeDate = '2020-05-01';
+      let beforeTime = '23:59';
+      let drinkGiven = 'false';
+      let moodFilter = '';
+      let orderBy = 0;
+
+      let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
+      report.should.have.lengthOf(4);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
+      for (let s of report) {
+        s.username.should.deep.equal('test1');
+        s.resName.should.deep.equal('Morris Moss');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
+      }
+    });
+
+    it('Get all contact sheets where the resident is calm between the dates and times', async() => {
+      let userFilter = '';
+      let resFilter = '';
+      let afterdate = '2000-01-01';
+      let afterTime = '00:00';
+      let beforeDate = '2020-05-01';
+      let beforeTime = '23:59';
+      let drinkGiven = 'false';
+      let moodFilter = 'calm';
+      let orderBy = 0;
+
+      let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
+      report.should.have.lengthOf(6);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
+      for (let s of report) {
+        s.mood.should.deep.equal('Calm');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
+      }
+    });
+
+    it('Get all contact sheets where the resident Morris Moss is relaxed between the dates and times', async() => {
+      let userFilter = '';
+      let resFilter = 'Morris Moss';
+      let afterdate = '2000-01-01';
+      let afterTime = '00:00';
+      let beforeDate = '2020-05-01';
+      let beforeTime = '23:59';
+      let drinkGiven = 'false';
+      let moodFilter = 'relaxed';
+      let orderBy = 0;
+
+      let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
+      report.should.have.lengthOf(12);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
+      for (let s of report) {
+        s.resName.should.deep.equal('Morris Moss');
+        s.mood.should.deep.equal('Relaxed');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
+      }
+    });
+
+    it('Get all contact sheets recorded by user test2 where the resident Morris Moss is relaxed between the dates and times', async() => {
+      let userFilter = 'test2';
+      let resFilter = 'Morris Moss';
+      let afterdate = '2000-01-01';
+      let afterTime = '00:00';
+      let beforeDate = '2020-05-01';
+      let beforeTime = '23:59';
+      let drinkGiven = 'false';
+      let moodFilter = 'relaxed';
+      let orderBy = 0;
+
+      let report = await reportModel.getContact(userFilter, resFilter, afterdate, afterTime, beforeDate, beforeTime, drinkGiven, moodFilter, orderBy);
+      report.should.have.lengthOf(4);
+      let start = new Date(afterdate + ' ' + afterTime);
+      let end = new Date(beforeDate + ' ' + beforeTime);
+      let csDate;
+      for (let s of report) {
+        s.username.should.deep.equal('test2');
+        s.resName.should.deep.equal('Morris Moss');
+        s.mood.should.deep.equal('Relaxed');
+        csDate = stringToDate(s.contactDate, s.contactTime);
+        csDate.should.be.greaterThan(start);
+        csDate.should.be.lessThan(end);
+      }
     });
   });
 });
